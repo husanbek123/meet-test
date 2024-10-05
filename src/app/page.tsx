8cc6622c6024c12fd274436ae0585b4628d95784
husanbek123 <husanbek555@gmail.com>
2024-10-05 16:35:43 +0500

    build fix try 3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,17 +3,23 @@ import { Button, Form, Input } from "antd";
 import { useParams, useSearchParams } from "next/navigation";
 import { Suspense } from "react";
 
+function Nimadir() {
+  const searchParams = useSearchParams();
+
+  return <h1>{JSON.stringify(searchParams)}</h1>;
+}
+
 export default function Home() {
   const params = useParams();
-  const searchParams = useSearchParams();
-  console.log(searchParams);
 
   return (
     <div className="p-7">
       <h1>Hello {params.name}</h1>
       Params:
       <h1>{JSON.stringify(params)}</h1>
-      <h1>{JSON.stringify(searchParams)}</h1>
+      <Suspense>
+        <Nimadir />
+      </Suspense>
     </div>
   );
 }
