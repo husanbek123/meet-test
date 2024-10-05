7175a24b30f8c3f0d763baf6d52be39cb63db452
husanbek123 <husanbek555@gmail.com>
2024-10-05 16:05:45 +0500

    Nimadir

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,17 +3,6 @@ import { Button, Form, Input } from "antd";
 import { useParams, useSearchParams } from "next/navigation";
 import { Suspense } from "react";
 
-function Search() {
-  const searchParams = useSearchParams();
-
-  return (
-    <>
-      Search/QueryParams:
-      <h1>{JSON.stringify(searchParams.entries())}</h1>
-    </>
-  );
-}
-
 export default function Home() {
   const params = useParams();
   const searchParams = useSearchParams();
@@ -24,9 +13,7 @@ export default function Home() {
       <h1>Hello {params.name}</h1>
       Params:
       <h1>{JSON.stringify(params)}</h1>
-      <Suspense>
-        <Search />
-      </Suspense>
+      <h1>{JSON.stringify(searchParams)}</h1>
     </div>
   );
 }
