2eba4e720a03c1fb5d9cfa0562d89e26a3c0b432
husanbek123 <husanbek555@gmail.com>
2024-10-05 12:33:33 +0500

    build fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,18 @@
 "use client";
 import { Button, Form, Input } from "antd";
 import { useParams, useSearchParams } from "next/navigation";
+import { Suspense } from "react";
+
+function Search() {
+  const searchParams = useSearchParams();
+
+  return (
+    <>
+      Search/QueryParams:
+      <h1>{JSON.stringify(searchParams.entries())}</h1>
+    </>
+  );
+}
 
 export default function Home() {
   const params = useParams();
@@ -11,8 +23,9 @@ export default function Home() {
       <h1>Hello {params.name}</h1>
       Params:
       <h1>{JSON.stringify(params)}</h1>
-      Search/QueryParams:
-      <h1>{JSON.stringify(searchParams.entries())}</h1>
+      <Suspense>
+        <Search />
+      </Suspense>
     </div>
   );
 }
