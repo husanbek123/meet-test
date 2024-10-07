02040265ad5891ec3d3970059f61723854b08504
husanbek123 <husanbek555@gmail.com>
2024-10-07 11:31:50 +0500

    token parse

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,28 @@ function Nimadir() {
   const paramsArr: any[] = [];
   searchParams.forEach((i) => paramsArr.push(i));
 
-  return <h1>{JSON.stringify(paramsArr)}</h1>;
+  const SplittedToken = searchParams.get("token")?.split(".") as any;
+
+  const tokenPayload1 = JSON.parse(
+    Buffer.from(SplittedToken[0], "base64").toString()
+  );
+
+  const tokenPayload2 = JSON.parse(
+    Buffer.from(SplittedToken[1], "base64").toString()
+  );
+
+  const tokenPayload3 = JSON.parse(
+    Buffer.from(SplittedToken[2], "base64").toString()
+  );
+
+  return (
+    <div>
+      <h1>{JSON.stringify(paramsArr)}</h1>
+      <div>{tokenPayload1}</div>
+      <div>{tokenPayload2}</div>
+      <div>{tokenPayload3}</div>
+    </div>
+  );
 }
 
 export default function Home() {
